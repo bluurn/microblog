a0901e34b95dc3d7f6b89cf4b7820bc86a88341e
Vovan <bluurn@gmail.com>
2014-04-21 19:07:41 +0400

    refactor specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,55 +4,36 @@ describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home Page" do
-    before { visit root_path}
+    subject { page }
+    before { visit root_path }
 
-    it "Should have content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    subject { page }
     before { visit help_path }
 
-    it "Should have content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "Should have title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help")}
   end
 
   describe "About page" do
+    subject { page }
     before { visit about_path }
 
-    it "Should have content 'About'" do
-      expect(page).to have_content('About')
-    end
-
-    it "Should have title 'About'" do
-      expect(page).to have_title("#{base_title} | About")
-    end
+    it { should have_content('About') }
+    it { should have_title("#{base_title} | About")}
   end
 
   describe "Contact page" do
+    subject { page }
     before { visit contact_path }
 
-    it "Should have content 'Contact'" do
-      expect(page).to have_content  ('Contacts')
-    end
-
-    it "Should have title 'Contact'" do
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title("#{base_title} | Contact")}
   end
 
 end
