9da76c204c15dd771e81489d00c5b105650fc6a6
Vovan <bluurn@gmail.com>
2014-04-21 16:10:56 +0400

    refactor spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
 
     it "Should have content 'Sample App'" do
@@ -11,7 +11,7 @@ describe "Static Pages" do
 
     it "Should have title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +24,7 @@ describe "Static Pages" do
 
     it "Should have title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +37,7 @@ describe "Static Pages" do
 
     it "Should have title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -50,7 +50,7 @@ describe "Static Pages" do
 
     it "Should have title 'Contacts'" do
       visit '/static_pages/contacts'
-      expect(page).to have_title('Contacts')
+      expect(page).to have_title("#{base_title} | Contacts")
     end
   end
 
