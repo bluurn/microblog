452c7da2b687a72a8b26e78afc8a5d9539f7dbf3
Vovan <bluurn@gmail.com>
2014-04-21 19:00:04 +0400

    add the before statement to spec descriptions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,59 +2,55 @@ require 'spec_helper'
 
 describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
+    before { visit root_path}
 
     it "Should have content 'Sample App'" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "Should have content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "Should have title 'Help'" do
-      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "Should have content 'About'" do
-      visit about_path
       expect(page).to have_content('About')
     end
 
     it "Should have title 'About'" do
-      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
     it "Should have content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content  ('Contacts')    
+      expect(page).to have_content  ('Contacts')
     end
 
     it "Should have title 'Contact'" do
-      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
