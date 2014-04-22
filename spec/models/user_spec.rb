d3cfb9addb566b3ab6d056d0b5b5e3be94d09f98
Vovan <bluurn@gmail.com>
2014-04-22 13:36:23 +0400

    add User#email format validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,8 +18,32 @@ describe User do
     before { @user.email = '' }
     it { should_not be_valid }
   end
+
   describe "when name is too long" do
     before { @user.name = 'a' * 51 }
     it { should_not be_valid }
+  end
+
+  describe "when email format should be valid" do
+    it "should be valid" do
+      
+      addresses = %w[some@company,com company.com some@company+sample.com]
+
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid       
+      end
+    end
+  end
+
+  describe "when email format should not be valid" do
+    it "should not be valid" do
+      addresses = %w[bluurn@gmail.com bluurn2@gmail.com]
+      
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid 
+      end
+    end
   end
 end
