f2432080e776b505b59bc28dcb526c574f48b71a
Vovan <bluurn@gmail.com>
2014-04-22 15:32:05 +0400

    add password min length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
     presence: true, 
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+  validates :password,
+            length: { minimum: 6 }
 
   before_save { self.email = email.downcase }
+  
   has_secure_password
 end
