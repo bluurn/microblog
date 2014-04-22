a6ebd4f949371dadf55dc151519e527f8f14108a
Vovan <bluurn@gmail.com>
2014-04-22 14:55:07 +0400

    add bcrypt add password digest to users add new user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false }
 
   before_save { self.email = email.downcase }
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new name: "Vladimir Suvorov", email: "bluurn@gmail.com" }
+  before do
+   @user = User.new name: "Vladimir Suvorov", 
+                            email: "bluurn@gmail.com",
+                            password: "foobar",
+                            password_confirmation: "foobar" 
+  end
   subject { @user } 
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -54,6 +62,21 @@ describe User do
       user_with_same_email.save
     end
     
+    it { should_not be_valid }
+  end
+
+  describe "when password is empty" do
+    before do
+     @user = User.new name: "Vladimir Suvorov", 
+                              email: "bluurn@gmail.com",
+                              password: "",
+                              password_confirmation: ""      
+    end
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 end
