ad04bb92193886d7ba5786fdcd8c7d7b3767fa75
Vovan <bluurn@gmail.com>
2014-04-23 16:21:36 +0400

    tidy up user mode code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, 
-    presence: true, 
+
+  validates :name,
+    presence: true,
+    length: { maximum: 50 }
+  validates :email,
+    presence: true,
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   validates :password,
-            length: { minimum: 6 }
+    length: { minimum: 6 }
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
@@ -24,8 +27,8 @@ class User < ActiveRecord::Base
 
   private
 
-    def create_remember_token
-      self.remember_token = User.digest(User.new_remember_token)
-    end
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
 
 end
