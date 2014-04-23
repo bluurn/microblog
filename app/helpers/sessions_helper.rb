9f9d2a3d2cf18f54b1c53cd1f5b69bb40ed2d1fc
Vovan <bluurn@gmail.com>
2014-04-23 16:24:43 +0400

    add SessionHelper#sign_out add SessionsController#destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,17 @@
 module SessionsHelper
+
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
     self.current_user = user
+  end
+
+  def sign_out
+    remember_token = User.new_remember_token
+    current_user.update_attribute(:remember_token, User.digest(remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 
   def current_user=(user)
