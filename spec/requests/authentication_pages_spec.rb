6730b2b95b27731ab59317589daa14336aca5e78
Vovan <bluurn@gmail.com>
2014-04-24 11:21:02 +0400

    add authorization spec for editing wrong users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,7 +31,7 @@ describe "Authentication" do
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
-    
+
     describe "authorization" do
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
@@ -46,6 +46,24 @@ describe "Authentication" do
             specify { expect(response).to redirect_to(signin_path) }
           end
         end
+      end
+
+      describe "as wrong user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@example.com') }
+        before { sign_in user, no_capibara: true }
+        
+        describe "submitting get request to Users#edit action" do
+          before { get edit_user_path(wrong_user) }
+          specify { expect(response.body).not_to match('Edit user') }
+          specify { expect(response).to redirect_to(root_url)  }
+        end
+
+        describe "submitting patch request to Users#update action" do
+          before { patch user_path(wrong_user) }          
+          specify { expect(response).to redirect_to(root_url)  }
+        end
+
       end
     end
   end
