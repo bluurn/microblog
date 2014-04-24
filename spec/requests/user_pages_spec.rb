56ded7379df2802cf4aadd1d02150b6ec96ebb12
Vovan <bluurn@gmail.com>
2014-04-24 16:25:49 +0400

    add user delete links spec add admin user to factory

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,6 +23,25 @@ describe "User pages" do
         end
       end
     end
+
+    describe "delete links" do
+      it { should_not have_link('delete')}      
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
   end
 
   describe "signup page" do
@@ -61,12 +80,14 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title('Sign up') }
   end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
   describe "edit page" do
     let(:user) { FactoryGirl.create(:user) }
     before do
