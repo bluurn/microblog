25b21f5580af3627f1d5e0cfae9acb8591878a3e
Vovan <bluurn@gmail.com>
2014-04-24 12:31:44 +0400

    Authentication SignIn page authorization for non-signed-in users when he tries to visit a protected page after signing in should render desired page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,6 +36,20 @@ describe "Authentication" do
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
 
+        describe "when he tries to visit a protected page" do
+          before do
+            visit edit_user_path(user)              
+            fill_in "Email", with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+
+          describe "after signing in" do
+            it "should render desired page" do
+              expect(page).to have_title 'Edit user'  
+            end
+          end
+        end
         describe "in the Users controller" do
           describe "visiting the edit page" do
             before { visit edit_user_path(user) }
@@ -52,7 +66,7 @@ describe "Authentication" do
         let(:user) { FactoryGirl.create(:user) }
         let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@example.com') }
         before { sign_in user }
-        
+
         describe "submitting get request to Users#edit action" do
           before { get edit_user_path(wrong_user) }
           specify { expect(response.body).not_to match('Edit user') }
@@ -60,12 +74,11 @@ describe "Authentication" do
         end
 
         describe "submitting patch request to Users#update action" do
-          before { patch user_path(wrong_user) }          
+          before { patch user_path(wrong_user) }
           specify { expect(response).to be_redirect  }
         end
 
       end
     end
   end
-
 end
