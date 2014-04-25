9185cd72748b0150fa2719fc368d6586248a5fd5
Vovan <bluurn@gmail.com>
2014-04-25 15:18:25 +0400

    move signed_in_user to sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,17 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def signed_in?
+    current_user.present?
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please, sign in"
+    end
+  end
+  
   def current_user=(user)
     @current_user = user
   end
@@ -21,10 +32,6 @@ module SessionsHelper
   def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
-  end
-
-  def signed_in?
-    current_user.present?
   end
 
   def current_user?(user)
