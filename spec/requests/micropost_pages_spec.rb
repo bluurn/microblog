6e63ab855875586e166782488181410d4a8ad655
Vovan <bluurn@gmail.com>
2014-04-25 17:24:39 +0400

    tidy up integration test add destroy micropost case

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -3,16 +3,14 @@ require 'spec_helper'
 describe "MicropostPages" do
   subject { page }
   let(:user) { FactoryGirl.create(:user) }
+  before { sign_in user }
 
   describe "micropost creation" do
-  	before do
-  		sign_in user
-  		visit root_path
-  	end
+    before { visit root_path }
 
     describe "with invalid information" do
       it "should not create micropost" do
-        expect { click_button "Post" }.not_to change(Micropost, :count)  
+        expect { click_button "Post" }.not_to change(Micropost, :count)
       end
 
       describe "error messages" do
@@ -25,7 +23,19 @@ describe "MicropostPages" do
       before { fill_in 'micropost_content', with: "Lorem Ipsum" }
 
       it "should create micropost" do
-        expect { click_button "Post" }.to change(Micropost, :count).by(1)  
+        expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+    end
+
+    describe "micropost destruction" do
+      before { FactoryGirl.create(:micropost, user: user) }
+
+      describe "as correct user" do
+        before { visit root_path }
+
+        it "should delete micropost" do
+          expect { click_link 'Delete' }.to change(Micropost, :count).by(-1)
+        end
       end
     end
   end
