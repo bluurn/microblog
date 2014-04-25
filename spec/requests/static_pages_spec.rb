da411be1f0db9dc2c9ef0103bb9f76302401a27f
Vovan <bluurn@gmail.com>
2014-04-25 16:44:32 +0400

    tidy up

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home Page" do
-    subject { page }
     before { visit root_path }
 
     it { should have_content('Sample App') }
@@ -13,7 +13,6 @@ describe "Static Pages" do
   end
 
   describe "Help page" do
-    subject { page }
     before { visit help_path }
 
     it { should have_content('Help') }
@@ -21,7 +20,6 @@ describe "Static Pages" do
   end
 
   describe "About page" do
-    subject { page }
     before { visit about_path }
 
     it { should have_content('About') }
@@ -29,11 +27,9 @@ describe "Static Pages" do
   end
 
   describe "Contact page" do
-    subject { page }
     before { visit contact_path }
 
     it { should have_content('Contact') }
     it { should have_title("#{base_title} | Contact")}
   end
-
 end
